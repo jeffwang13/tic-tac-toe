fa5ca6dd90d3e440a734c73c1377d3d8f9046bd0
Jeffrey Wang <jeffrey868@gmail.com>
2017-01-17 20:30:30 -0500

    update url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,8 +35,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-   config.action_cable.url = 'wss://pacific-chamber-80591.herokuapp.com/cable'
-   config.action_cable.allowed_request_origins = [ 'https://pacific-chamber-80591.herokuapp.com', /http:\/\/pacific-chamber-80591.herokuapp.com.*/  ]
+   config.action_cable.url = 'wss://jwtictactoe.herokuapp.com/cable'
+   config.action_cable.allowed_request_origins = [ 'https://jwtictactoe.herokuapp.com', /http:\/\/jwtictactoe.herokuapp.com.*/  ]
 
   # Don't mount Action Cable in the main server process.
   # config.action_cable.mount_path = nil
